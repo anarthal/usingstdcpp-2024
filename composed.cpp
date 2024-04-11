557313fe135dd8ea2ba7130b464b161005b0c578
Ruben Perez <rubenperez038@gmail.com>
2024-04-11 13:14:09 +0200

    Final composed

--- composed.cpp
+++ composed.cpp
@@ -1,7 +1,5 @@
 
 #include <boost/asio/any_io_executor.hpp>
-#include <boost/asio/as_tuple.hpp>
-#include <boost/asio/async_result.hpp>
 #include <boost/asio/awaitable.hpp>
 #include <boost/asio/buffer.hpp>
 #include <boost/asio/co_spawn.hpp>
@@ -9,7 +7,6 @@
 #include <boost/asio/connect.hpp>
 #include <boost/asio/deferred.hpp>
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/ip/address.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/read_until.hpp>
 #include <boost/asio/this_coro.hpp>
@@ -24,33 +21,18 @@
 namespace asio = boost::asio;
 using boost::system::error_code;
 
+// The GET HTTP request to send to the server
 static constexpr std::string_view request =
     "GET / HTTP/1.1\r\n"
-    "Host: www.python.org\r\n"
-    "User-Agent: curl/7.71.1\r\n"
+    "Host: example.com\r\n"
+    "User-Agent: Asio\r\n"
     "Accept: */*\r\n\r\n";
 
-void read_headers_v1(
-    asio::ip::tcp::socket& sock,
-    std::string_view req,
-    std::string& buff,
-    std::function<void(error_code, std::size_t)> h
-)
+// Bonus: this is how you write a generic composed operation
+struct handle_request_op
 {
-    asio::async_write(
-        sock,
-        asio::buffer(req),
-        [&sock, &buff, h = std::move(h)](error_code ec, std::size_t) mutable {
-            if (ec)
-                h(ec, 0u);
-            else
-                asio::async_read_until(sock, asio::dynamic_buffer(buff), "\r\n\r\n", h);
-        }
-    );
-}
-
-struct read_headers_op
-{
+    // Operation state
+    asio::ip::tcp::resolver& resolv;
     asio::ip::tcp::socket& sock;
     std::string_view req;
     std::string& buff;
@@ -58,77 +40,103 @@ struct read_headers_op
     enum class state_t
     {
         initial,
+        resolving,
+        connecting,
         writing,
-        reading
+        reading,
     } state{state_t::initial};
 
+    // Called when the operation is initiated
     template <class Self>
-    void operator()(Self& self, error_code ec = {}, std::size_t bytes_transferred = 0)
+    void operator()(Self& self)
     {
-        switch (state)
+        assert(state == state_t::initial);
+        state = state_t::resolving;
+        resolv.async_resolve("example.com", "80", std::move(self));
+    }
+
+    // Called when async_resolve completes
+    template <class Self>
+    void operator()(Self& self, error_code ec, asio::ip::tcp::resolver::results_type endpoints)
+    {
+        if (ec)
+            return self.complete(ec, 0u);
+        assert(state == state_t::resolving);
+        state = state_t::connecting;
+        asio::async_connect(sock, endpoints, std::move(self));
+    }
+
+    // Called when async_connect completes
+    template <class Self>
+    void operator()(Self& self, error_code ec, asio::ip::tcp::endpoint)
+    {
+        if (ec)
+            return self.complete(ec, 0u);
+        assert(state == state_t::connecting);
+        state = state_t::writing;
+        asio::async_write(sock, asio::buffer(request), std::move(self));
+    }
+
+    // Called when async_write and async_write complete
+    template <class Self>
+    void operator()(Self& self, error_code ec, std::size_t bytes_transferred)
+    {
+        if (ec)
+            return self.complete(ec, 0u);
+
+        if (state == state_t::writing)
         {
-        case state_t::initial:
-            state = state_t::writing;
-            asio::async_write(sock, asio::buffer(req), std::move(self));
-            break;
-        case state_t::writing:
-            if (ec)
-            {
-                self.complete(ec, 0u);
-            }
-            else
-            {
-                state = state_t::reading;
-                asio::async_read_until(sock, asio::dynamic_buffer(buff), "\r\n\r\n", std::move(self));
-            }
-            break;
-        case state_t::reading: self.complete(ec, bytes_transferred); break;
+            state = state_t::reading;
+            asio::async_read_until(sock, asio::dynamic_buffer(buff), "\r\n\r\n", std::move(self));
+        }
+        else
+        {
+            assert(state == state_t::reading);
+            self.complete(error_code(), bytes_transferred);
         }
     }
 };
 
 template <asio::completion_token_for<void(error_code, std::size_t)> CompletionToken>
-auto read_headers_v2(
+auto handle_request_generic(
+    asio::ip::tcp::resolver& resolv,
     asio::ip::tcp::socket& sock,
     std::string_view req,
     std::string& buff,
     CompletionToken&& token
 )
 {
+    // Takes care of the completion token return type transformation,
+    // propagates the associated characteristics...
     return asio::async_compose<CompletionToken, void(error_code, std::size_t)>(
-        read_headers_op{sock, req, buff},
+        handle_request_op{resolv, sock, req, buff},
         token,
+        resolv,
         sock
     );
 }
 
-asio::awaitable<error_code> handle_request_impl()
+asio::awaitable<void> handle_request_impl()
 {
+    // Get the executor
     asio::any_io_executor ex = co_await asio::this_coro::executor;
 
+    // These variables need to remain valid until the operation completes
     asio::ip::tcp::socket sock(ex);
     asio::ip::tcp::resolver resolv(ex);
-
     std::string buff;
 
-    auto [ec1, endpoints] = co_await resolv.async_resolve("python.org", "80", asio::as_tuple(asio::deferred));
-    if (ec1)
-        co_return ec1;
-    auto [ec2, ep] = co_await asio::async_connect(sock, endpoints, asio::as_tuple(asio::deferred));
-    if (ec2)
-        co_return ec2;
-
-    auto [ec3, bytes_read] = co_await read_headers_v2(sock, request, buff, asio::as_tuple(asio::deferred));
-    if (ec3)
-        co_return ec3;
-
+    // Call our operation
+    std::size_t bytes_read = co_await handle_request_generic(resolv, sock, request, buff, asio::deferred);
     std::cout << std::string_view(buff.data(), bytes_read) << std::endl;
-    co_return error_code();
 }
 
 void handle_request(asio::any_io_executor ex)
 {
-    asio::co_spawn(ex, handle_request_impl, [](...) {});
+    asio::co_spawn(ex, handle_request_impl, [](std::exception_ptr exc) {
+        if (exc)
+            std::rethrow_exception(exc);
+    });
 }
 
 int main()
